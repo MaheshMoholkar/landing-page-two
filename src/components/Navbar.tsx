96019731eb24e89bce374b2780f8b7b549432c08
Mahesh Moholkar <maheshmoholkar171@gmail.com>
2025-07-03 20:52:38 +0530

    add header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,54 @@
+import Image from "next/image";
+import Logo from "@/assets/images/logosaas.png";
+import MenuIcon from "@/assets/icons/menu.svg";
+
 export const Navbar = () => {
-  return null;
+  return (
+    <div className="bg-black">
+      <div className="px-4">
+        <div className="flex items-center justify-between py-5">
+          <div className="relative">
+            <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,rgba(252,214,255,1),rgba(41,216,255,1),rgba(255,253,128,1),rgba(248,154,191,1),rgba(252,214,255,1))] blur-md"></div>
+            <Image src={Logo} alt="Saas Logo" className="h-12 w-12 relative" />
+          </div>
+          <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden">
+            <MenuIcon className="text-white" />
+          </div>
+          <nav className="hidden sm:flex gap-6 items-center">
+            <a
+              href="#about"
+              className="text-white text-opacity-60  hover:text-opacity-100 transition"
+            >
+              About
+            </a>
+            <a
+              href="#features"
+              className="text-white text-opacity-60  hover:text-opacity-100 transition"
+            >
+              Features
+            </a>
+            <a
+              href="#cta"
+              className="text-white text-opacity-60  hover:text-opacity-100 transition"
+            >
+              Updates
+            </a>
+            <a
+              href="#cta"
+              className="text-white text-opacity-60  hover:text-opacity-100 transition"
+            >
+              Help
+            </a>
+            <a
+              href="#testimonials"
+              className="text-white text-opacity-60  hover:text-opacity-100 transition"
+            >
+              Customers
+            </a>
+            <button className="btn btn-primary">Get for free</button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
